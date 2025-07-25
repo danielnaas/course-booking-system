016960e59c9ac21710e46c4f3574730f98c1e742
Daniel Naas <daniel.naas@uky.edu>
2025-07-25 15:24:20 -0400

    End of section 1

---
+++ course-booking-system/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CoursesListComponent } from "./courses-list/courses-list.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CoursesListComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'course-booking-system';
+}
