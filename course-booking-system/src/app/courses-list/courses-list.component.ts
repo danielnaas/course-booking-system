04ccd24866bf8c07abe5dc04ebf0926d9c4163a8
Daniel Naas <daniel.naas@uky.edu>
2025-08-04 13:29:33 -0400

    Separating course module

--- course-booking-system/src/app/courses-list/courses-list.component.ts
+++ course-booking-system/src/app/courses-list/courses-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseCardComponent } from '../course-card/course-card.component';
-
+import { Course } from '../models/course.model';
 
 @Component({
   selector: 'app-courses-list',
@@ -10,8 +10,8 @@ import { CourseCardComponent } from '../course-card/course-card.component';
 })
 export class CoursesListComponent implements OnInit {
   title: string = "Available courses";
-  wishlist: any[] = [];
-  courses = [
+  wishlist: Course[] = [];
+  courses: Course[] = [
     { id: 1, title: 'Intro to Angular', description: 'Learn the basics of Angular', price: 49, date: '2025-03-01', soldOut: false, img: 'angular-logo.png', onSale: false },
     { id: 2, title: 'Advanced Angular', description: 'Deep dive into Angular internals', price: 99, date: '2025-04-10', soldOut: true, img: 'angular-logo.png', onSale: true },
     { id: 3, title: 'RxJS Fundamentals', description: 'Asynchronous data streams', price: 45, date: '2025-05-05', img: 'rxjs-logo.png', soldOut: false, onSale: true }
@@ -21,15 +21,13 @@ export class CoursesListComponent implements OnInit {
     console.log("CoursesList initialized!")
   }
 
-  onCourseBooked(course: any): void {
+  onCourseBooked(course: Course): void {
     console.log('Parent heard about booking:', course.title);
     // Potentially do more here in the future
   }
 
-  onWishlistAdded(course: any): void {
-    console.log('Add this to wish list:', course.title);
+  onWishlistAdded(course: Course): void {
+    console.log('Wishlist event triggered for:', course.title);
     this.wishlist.push(course);
   }
-
- 
 }
